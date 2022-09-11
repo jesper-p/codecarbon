8b6b215f03a2f1a92ec18bdec82814ec7310d9ce
Jesper Petersson <jesperpetersson97@gmail.com>
2022-09-11 12:12:55 +0200

    test

--- codecarbon/core/emissions.py
+++ codecarbon/core/emissions.py
@@ -34,7 +34,9 @@ class Emissions:
 
         df: pd.DataFrame = self._data_source.get_cloud_emissions_data()
 
-        print((df["provider"] == cloud.provider) & (df["region"] == cloud.region))
+        print(df.loc[(df["provider"] == cloud.provider) & (df["region"] == cloud.region)][
+                "impact"
+            ])
 
         emissions_per_kWh: EmissionsPerKWh = EmissionsPerKWh.from_g_per_kWh(
             df.loc[(df["provider"] == cloud.provider) & (df["region"] == cloud.region)][
